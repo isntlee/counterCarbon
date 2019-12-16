19f2a6a133b290c612e53a8d64fb2802d385ae0e
Space-Rex <lmartina@tcd.ie>
2019-12-16 11:44:48 +0000

    Addin to graphs.js

--- javascript/graphs.js
+++ javascript/graphs.js
@@ -1,6 +1,8 @@
 
 
 
+   
+   
 document.addEventListener('DOMContentLoaded', function () {
     var graphMobility = Highcharts.chart('graphMobility', {
     chart: {
@@ -59,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function () {
     series: [{
         name: 'Per capita CO2 emissions',
         color: 'rgba(165,170,217,1)',
-        data: [60, 77, 85, 91],
+        data: [2.5, 3.1, 2.2, 1.6],
         pointPadding: -0.2,
         pointPlacement: 0.0
     }, {
@@ -68,13 +70,29 @@ document.addEventListener('DOMContentLoaded', function () {
         borderWidth: 3, 
         borderColor:'white',
         dashStyle: 'ShortDot', 
-        data: [40],
+        data: [0.0],
         pointPadding: -0.2,
         pointPlacement: 0.0
         }]
     });
-});
 
+    var sum = 0;
+    
+    $("#mobilityBox input:radio").click(function() {
+       sum = 0.8;
+        
+        $("#mobilityBox input:radio:checked").each(function(idx, elm) {
+            sum += parseFloat(elm.value, 10);
+        });
+        
+        var mobilityTotal = sum;
+        
+        graphMobility.series[1].setData([sum], true);
+        
+      });
+    });
+    
+    
 document.addEventListener('DOMContentLoaded', function () {
     var graphFood = Highcharts.chart('graphFood', {
     chart: {
@@ -132,7 +150,7 @@ document.addEventListener('DOMContentLoaded', function () {
     series: [{
         name: 'Per capita CO2 emissions',
         color: 'rgba(165,170,217,1)',
-        data: [60, 77, 85, 91],
+        data: [4.5, 5.1, 3.9, 5.2],
         pointPadding: -0.2,
         pointPlacement: 0.0
     }, {
@@ -141,12 +159,30 @@ document.addEventListener('DOMContentLoaded', function () {
         borderWidth: 3, 
         borderColor:'white',
         dashStyle: 'ShortDot', 
-        data: [40],
+        data: [0.0],
         pointPadding: -0.2,
         pointPlacement: 0.0
         }]
     });
-});
+
+
+    var sum = 0;
+    
+    
+    $("#foodBox input:radio").click(function() {
+       sum = 1.7;
+        
+        $("#foodBox input:radio:checked").each(function(idx, elm) {
+            sum += parseFloat(elm.value, 10);
+        });
+        
+        var foodTotal = sum;
+        
+        graphFood.series[1].setData([sum], true);
+        
+      });
+    });
+
 
 document.addEventListener('DOMContentLoaded', function () {
     var graphHousehold = Highcharts.chart('graphHousehold', {
@@ -205,7 +241,7 @@ document.addEventListener('DOMContentLoaded', function () {
     series: [{
         name: 'Per capita CO2 emissions',
         color: 'rgba(165,170,217,1)',
-        data: [60, 77, 85, 91],
+        data: [7.1, 7.4, 8.4, 7.1],
         pointPadding: -0.2,
         pointPlacement: 0.0
     }, {
@@ -214,12 +250,32 @@ document.addEventListener('DOMContentLoaded', function () {
         borderWidth: 3, 
         borderColor:'white',
         dashStyle: 'ShortDot', 
-        data: [40],
-        pointPadding: -0.2,
+        data: [0.0],
+        pointPadding: -0.2,  
         pointPlacement: 0.0
         }]
+       
     });
-});
+  
+
+    var sum = 0;
+    
+    
+    $("#householdBox input:radio").click(function() {
+       sum = 0.1;
+       
+       
+        
+        $("#householdBox input:radio:checked").each(function(idx, elm) {
+            sum += parseFloat(elm.value, 10);
+        });
+        
+        var householdTotal = sum;
+        
+        graphHousehold.series[1].setData([sum], true);
+        
+      });
+    });
 
 
 
@@ -247,13 +303,13 @@ document.addEventListener('DOMContentLoaded', function () {
     },
     
     title: {
-        text: 'Mobility: 3t <br> Total: 11t ', 
+        text: 'Household: 3t <br> Total: 11t ', 
         align: 'center',
         verticalAlign: 'middle',
         y: 35, 
         style: {
-				fontSize: '3.2vh',
-				color: '#505050', 
+				fontSize: '3.1vh',
+				color: '#404040', 
 				fontWeight: '600', 
 				lineHeight: '45', 
 				fontFamily: 'Verdana'
@@ -282,15 +338,17 @@ document.addEventListener('DOMContentLoaded', function () {
     series: [{
         type: 'pie',
         name: 'Emissions share',
-        innerSize: '50%',
+        innerSize: '53%',
         data: [
-            ['Mobility', 58.9],
-            ['Food', 13.29],
-            ['Household', 13],
+            ['Mobility', 28.9],
+            ['Food', 23.29],
+            ['Household', 33.65],
         ]
     }]
 });
 });
+
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -376,3 +434,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 });
+ 
+
+
+
