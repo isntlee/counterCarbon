92210638def1956875d55094628923e6e5905f91
Space-Rex <lmartina@tcd.ie>
2020-01-20 21:42:53 +0000

    HTML/CSS: Bootstrap hamburger is working fully

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -10,29 +10,34 @@ $(document).ready(function(){
                     
                 } 
                 if(scroll_pos > 690) {
-                    
                     $("nav").removeClass("hidden").addClass("visible");
                     $("nav a").removeClass("hidden").addClass("visible");
                     $("nav").css('background-color', 'transparent');
                     $("nav a").css('color', '#404040');
-                    
-                } else {
+                    $('nav span').css('color','#404040');
+                }
+                else {
                     $("nav").css('background-color', 'transparent');
                     $("nav a").css('color', 'transparent');
                 }
                 if(scroll_pos > 1270) {
                     $("nav a").css('color', '#33adff');
+                    $("nav span").css('color', '#33adff');
                 }
                 if(scroll_pos > 1290) {
                     $("nav").css('background-color', '#fafafa');
                 }
+                
                 if(scroll_pos < 50) {
                     $("nav").css('background-color', 'transparent');
                     $("nav a").css('color', '#fafafa');
+                    $('nav span').css('color','#fafafa');
                     $("nav").removeClass("hidden").addClass("visible");
                     $("nav a").removeClass("hidden").addClass("visible");
                 }
             });
         });
+        
+    
         
                             //THIS IS A FUCKING MESS, BUT IT WORKS//
